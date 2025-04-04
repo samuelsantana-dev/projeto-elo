de5a25da68db0bc0a2f67ae9dfefa8f8384656dd
samuelsantana-dev <samuelsantanadasilva8@gmail.com>
2025-04-04 15:19:08 -0300

    Section de fotos

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import BoxComponent from './pages/on'
+import FranchiseSteps from './pages/on'
 import FranchiseBox from './pages/franchisees'
 import FooterSection from './pages/footer'
 import Box1 from './pages/header'
+import FAQ from './pages/faq'
+import Contact from './pages/contact'
+import Photos from './pages/photos'
+// import Feedbacks from './pages/feedbacks'
 
 
 function App() {
@@ -9,8 +13,12 @@ function App() {
   return (
     <>
         {/* <Box1 /> */}
-        {/* <BoxComponent />
-        <FranchiseBox /> */}
+        <FranchiseSteps />
+        {/* <FranchiseBox /> */}
+        {/* <Feedbacks /> */}
+        <Photos />
+        <FAQ />
+        <Contact />
         <FooterSection />
     </>
   )
