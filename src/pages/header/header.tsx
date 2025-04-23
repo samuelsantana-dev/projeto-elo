da0d80db7ccba3662b0f89a4f088aa0496ec623a
samuelsantana-dev <samuelsantanadasilva8@gmail.com>
2025-04-23 18:59:25 -0300

    Modelo header ajustado espaçamento dos botões

--- src/pages/header/header.tsx
+++ src/pages/header/header.tsx
@@ -6,6 +6,13 @@ const BrotegePiso = () => {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
+  const phoneNumber = '+55 (11) 99999-9999';
+
+  const handleWhatsAppClick = () => {
+    window.open(`https://wa.me/${phoneNumber}`, "_blank");
+  };
+
+
   return (
     <Box sx={{
       minHeight: '100vh',
@@ -60,41 +67,53 @@ const BrotegePiso = () => {
           <a href="/contato" style={{ textDecoration: 'none', color: '#000000', }}>Contato</a>
         </Box>
 
-        <Box sx={{ display: isMobile ? 'none' : 'flex', alignItems: 'center' }}>
-          <Button
-            variant="outlined"
-            color="success"
-            sx={{
-              borderColor: '#00ff00',
-              color: '#000000',
-              '&:hover': { borderColor: '#00cc00', backgroundColor: 'rgba(0,204,0,0.1)' },
-              marginRight: '20px',
-            }}
-          >
-            Seja um Franqueado
+        <Box sx={{ display: isMobile ? 'none' : 'flex', alignItems: 'center', marginBottom: '20px'  }}>
+          <Button variant="contained" sx={{
+            backgroundColor: '#00ff00',
+            color: '#fff',
+            fontWeight: 'bold',
+            padding: '12px 12px',
+            fontSize: isMobile ? '1rem' : '0.8rem',
+            borderRadius: '8px',
+            boxShadow: '0px 4px 8px rgba(0,0,0,0.2)',
+            '&:hover': {
+              backgroundColor: '#00cc00',
+              transform: 'scale(1.05)',
+              boxShadow: '0px 6px 12px rgba(0,0,0,0.3)',
+            },
+            transition: 'all 0.3s ease',
+            marginBottom: '20px'
+          }}>
+            Seja um <strong> Franqueado</strong>
           </Button>
           <Box
-            component="a"
-            href="https://wa.me/SEU_NUMERO_DE_WHATSAPP"
-            target="_blank"
-            rel="noopener noreferrer"
+            onClick={handleWhatsAppClick}
             sx={{
-              width: '30px',
-              height: '30px',
+               marginBottom: '20px',
+              width: '50px',
+              height: '50px',
               borderRadius: '50%',
               backgroundColor: '#25D366',
               display: 'flex',
               justifyContent: 'center',
               alignItems: 'center',
-              color: '#fff',
-              fontSize: '1.2rem',
               cursor: 'pointer',
+                marginLeft: '10px',
+              transition: 'background-color 0.2s',
               '&:hover': {
                 backgroundColor: '#128C7E',
               },
             }}
           >
-            <Typography variant="body2">WA</Typography>
+            <svg 
+              xmlns="http://www.w3.org/2000/svg" 
+              width="20" 
+              height="20" 
+              viewBox="0 0 24 24" 
+              fill="white"
+            >
+              <path d="M17.472 14.382c-.297-.149-1.758-.867-2.03-.967-.273-.099-.471-.148-.67.15-.197.297-.767.966-.94 1.164-.173.199-.347.223-.644.075-.297-.15-1.255-.463-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.298-.347.446-.52.149-.174.198-.298.298-.497.099-.198.05-.371-.025-.52-.075-.149-.669-1.612-.916-2.207-.242-.579-.487-.5-.669-.51-.173-.008-.371-.01-.57-.01-.198 0-.52.074-.792.372-.272.297-1.04 1.016-1.04 2.479 0 1.462 1.065 2.875 1.213 3.074.149.198 2.096 3.2 5.077 4.487.709.306 1.262.489 1.694.625.712.227 1.36.195 1.871.118.571-.085 1.758-.719 2.006-1.413.248-.694.248-1.289.173-1.413-.074-.124-.272-.198-.57-.347m-5.421 7.403h-.004a9.87 9.87 0 01-5.031-1.378l-.361-.214-3.741.982.998-3.648-.235-.374a9.86 9.86 0 01-1.51-5.26c.001-5.45 4.436-9.884 9.888-9.884 2.64 0 5.122 1.03 6.988 2.898a9.825 9.825 0 012.893 6.994c-.003 5.45-4.437 9.884-9.885 9.884m8.413-18.297A11.815 11.815 0 0012.05 0C5.495 0 .16 5.335.157 11.892c0 2.096.547 4.142 1.588 5.945L.057 24l6.305-1.654a11.882 11.882 0 005.683 1.448h.005c6.554 0 11.89-5.335 11.893-11.893a11.821 11.821 0 00-3.48-8.413Z"/>
+            </svg>
           </Box>
         </Box>
 
@@ -169,7 +188,6 @@ const BrotegePiso = () => {
         </Typography>
       </Box>
 
-      {/* Gradiente (Flex Item - posicionado no final com flex-grow) */}
       <Box sx={{
         width: '100%',
         height: '40%',
